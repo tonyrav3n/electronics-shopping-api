42edc2d8cf759c0f7536ca54a7e36e240ab91b75
tonyraven <tonyraven.dev@gmail.com>
2026-06-24 02:23:17 +0100

    Refactor DB init script and tests

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -27,14 +27,20 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Launch postgres using Docker
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-    docker run \
-        -e POSTGRES_USER=${DB_USER} \
-        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-        -e POSTGRES_DB=${DB_NAME} \
-        -p "${DB_PORT}":5432 \
-        -d postgres \
-        postgres -N 1000
-        # ^ Increased maximum number of connections for testing purposes
+    if docker container inspect electronics-db >/dev/null 2>&1; then
+        echo >&2 "Starting existing database container..."
+        docker start electronics-db
+    else
+        docker run \
+            --name electronics-db \
+            -e POSTGRES_USER=${DB_USER} \
+            -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+            -e POSTGRES_DB=${DB_NAME} \
+            -p "${DB_PORT}":5432 \
+            -d postgres \
+            postgres -N 1000
+            # ^ Increased maximum number of connections for testing purposes
+    fi
 fi
 
 export PGPASSWORD="${DB_PASSWORD}"
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -20,7 +20,7 @@ async fn spawn_app() -> TestApp {
     let server = electronics_shopping_api::startup::run(listener, connection_pool.clone())
         .expect("Failed to bind address");
 
-    let _ = tokio::spawn(server);
+    tokio::spawn(server);
 
     TestApp {
         address,
@@ -30,40 +30,33 @@ async fn spawn_app() -> TestApp {
 
 #[tokio::test]
 async fn health_check_returns_200() {
-    // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
 
-    // Act
     let response = client
-        .get(&format!("{}/health_check", &app.address))
+        .get(format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("Failed to execute request.");
 
-    // Assert
     assert!(response.status().is_success());
     assert_eq!(200, response.status().as_u16());
 }
 
 #[tokio::test]
 async fn add_product_returns_200_for_valid_form_data() {
-    // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-    
-    // Using URL-encoded form data
+
     let body = "name=PlayStation%205&brand=Sony&description=Gaming%20Console&price=499&stock=10";
 
-    // Act
     let response = client
-        .post(&format!("{}/products", &app.address))
+        .post(format!("{}/products", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
         .expect("Failed to execute request.");
 
-    // Assert
     assert_eq!(200, response.status().as_u16());
 }
